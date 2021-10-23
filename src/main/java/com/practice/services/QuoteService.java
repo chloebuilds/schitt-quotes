1b73245b799d563ddcd3a823de7e6dda4af089aa
Chloe Brown <chloemail@protonmail.com>
2021-10-23 21:24:33 +0100

    oh my god - david rose

--- src/main/java/com/practice/services/QuoteService.java
+++ src/main/java/com/practice/services/QuoteService.java
@@ -1,6 +1,7 @@
 package com.practice.services;
 
 import com.practice.models.Quote;
+import com.practice.models.SCCharacter;
 import com.practice.repos.QuoteRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
